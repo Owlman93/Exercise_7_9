7b9759bfec61f790953295142f1295923d484aba
Owlman93 <74562731+Owlman93@users.noreply.github.com>
2020-11-20 19:03:28 +0100

    Update digits.cs

--- digits.cs
+++ digits.cs
@@ -1,35 +1,40 @@
+/*
+*(c) IES San Vicente 2020
+*
+*Program that calculates how many numbers of 1, 2, 3 or more digits are entered by the User until it types "end"
+*/
+
 using System;
 
-class digits
-{
- static void Main()
- {
-	 //counters of number of digits a is for one digit, b is for 2 digits, c is for 3 digits and d is for more than 3 digits
-     int a = 0, b = 0, c = 0, d = 0;
-      Console.Write("Insert a number: ");
-      // read the number as string to check the word "end"
-      string e = Console.ReadLine();
-     while(e!="end"){
-int input = Convert.ToInt32(e);
-       if(input != 0)
-{
-       if(input/10 == 0)
-           a ++;  // it only has one digit
-       else 
-       if (input/100 == 0)
-		b ++;  // the number has 2 digits
-        else
-        if (input/1000 == 0)
-        c ++;  // the number has 3 digits
-        else
-           d++;  // the number has more than 3 digits
-        }
-      e = Console.ReadLine();
-}
-
-Console.WriteLine("one: {0} two: {1} three: {2} more: {3}", a, b, c, d); 
-
-
- }
-}
-
+	class digits
+	{
+		 static void Main()
+ 		{
+     			int single = 0, doble = 0, triple = 0, multiple = 0;
+      			Console.Write("Insert a number: ");
+      			string e = Console.ReadLine();
+			 
+     			while(e != "end")
+     			{
+				int input = Convert.ToInt32(e);
+     				
+				if(input != 0)
+				{
+       					if(input/10 == 0)
+           				single ++;
+       					
+					else if (input/100 == 0)
+					doble ++;
+        
+					else if (input/1000 == 0)
+        				triple ++;
+        
+					else
+           				multiple++;
+        			}
+      				e = Console.ReadLine();
+			}
+			 
+			Console.WriteLine("one: {0} two: {1} three: {2} more: {3}", single, doble, triple, multiple); 
+		}
+	}
